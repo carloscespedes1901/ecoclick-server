7a148ea2f5a71efc5bb7a10cf6c826181c8716bf
Carlos Céspedes <106044240+carloscespedes1901@users.noreply.github.com>
2023-07-19 10:17:16 -0400

    Add files via upload

--- inventario-ecoclick-back/src/main/java/com/ecoclick/security/WebSecurityConfig.java
+++ inventario-ecoclick-back/src/main/java/com/ecoclick/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ecoclick.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,6 +39,7 @@ public class WebSecurityConfig {
 				.and()
 				.csrf().disable()
 				.authorizeHttpRequests()
+				.requestMatchers(HttpMethod.POST, "/login").permitAll()
 				.requestMatchers(new AntPathRequestMatcher("/usuario/recoverPassword")).permitAll()
 				.anyRequest()
 				.authenticated()
